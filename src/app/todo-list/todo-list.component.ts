5f40b8c2a35b233802fe2d9c05e32837bbc764c9
Szymon Mielnik <mielniks@zsl230.local>
2024-10-29 11:05:11 +0100

    ngfor

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
+import {NgForOf} from '@angular/common';
+import {ToDo} from '../shared/interfaces/todo.interface';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 
 export class TodoListComponent {
-  todos: string[] = [];
+  todos: ToDo[] = [];
 
   addTodo(todo: string): void {
-    this.todos.push(todo);
-    console.log('Aktualna lista todo: ', this.todos);
+    this.todos.push({name: todo, isComplete: false});
+    console.log(this.todos);
   }
 }
